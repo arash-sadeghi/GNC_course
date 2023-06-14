89d26c2ac0da3c0611182ed362f884b9f2cb94e0
arash sadeghi <arash.sadeghi1997@gmail.com>
2023-06-14 04:50:12 +0000

    ex2 finished

--- ex2/src/wallAvoider.py
+++ ex2/src/wallAvoider.py
@@ -2,6 +2,8 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from pdb import set_trace
+import numpy as np
 class wallAvoider:
     def __init__(self):
         rospy.init_node('wallAvoider')
@@ -11,23 +13,27 @@ class wallAvoider:
         self.move = Twist()
 
     def subCallback(self,msg):
-        print(msg)
+        # print(msg)
+        # find index of right and left:
+        # np.count_nonzero(np.array(np.where(np.isinf(x)))<=234)
+        # 90 degree is front of robot. 
+        msgArray = np.array(msg.ranges)
+        # print(msgArray.shape)
+        right = msgArray[0:235] 
+        center = msgArray[235:485] 
+        # left = msgArray[485:] 
+        # set_trace()
 
-        self.moveForward()
-        self.rate.sleep()
-
-        self.rotate('right')
-        self.rate.sleep()
-
-        self.moveForward()
-        self.rate.sleep()
-
-        self.rotate('right')
-        self.rate.sleep()
-
-        self.stop()
-        self.rate.sleep()
-
+        if np.any(center<= 1) :
+            print("detected on center")
+            self.stop()
+            self.rotate('left')
+        elif np.any(right<= 1):
+            print("detected on right")
+            self.rotate('left')
+        else:
+            print("move forward")
+            self.moveForward()
 
     def stop(self):
         self.move.linear.x = 0
